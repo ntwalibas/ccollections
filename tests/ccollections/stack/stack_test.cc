f1f84ec0161466e091b428cc80b26c366baada3b
Ntwali B <ntwali.bashige@gmail.com>
2022-12-02 17:38:22 +0100

    Modified tests to use fixtures.

--- tests/ccollections/stack/stack_test.cc
+++ tests/ccollections/stack/stack_test.cc
@@ -5,17 +5,31 @@ extern "C" {
     #include "src/ccollections/stack/stack.h"
 }
 
+class StackTest: public ::testing::Test {
+    protected:
+        void SetUp() override {
+            char const * message = "Stack initialization failed.";
+            stack = newStack(10, message);
+        }
+
+        void TearDown() override {
+            deleteStack(&stack);
+        }
+    
+        struct Stack * stack;
+};
 
 // Make sure a stack of the given size is created upon request
-TEST(StackTestSuite, newStackTest) {
-    char const * message = "Stack initialization failed.";
-    struct Stack * stack = newStack(10, message);
+TEST_F(StackTest, newStackTest) {
     // Expect that a new stack was created
     EXPECT_NE(stack, nullptr);
 
     // Expect that the stack size is equal to 10
     EXPECT_EQ(stack -> size, 10);
+}
 
-    // Expect that in case of failure, the correct message was set
-    EXPECT_STREQ(stack -> message, message);
+// Make sure the stack is freed upon calling deleteStack
+TEST_F(StackTest, deleteStackTest) {
+    deleteStack(&stack);
+    EXPECT_EQ(stack, nullptr);
 }
